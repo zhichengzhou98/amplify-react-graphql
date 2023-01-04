7ce2e0b1268f4d4dde1f116fa784c852f2c6dde9
renren <renren@zhichengzhou-MacBook-Air.local>
2023-01-04 13:45:11 +0800

    111

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyapp41a1dab4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello renren</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <useAuthenticator />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
