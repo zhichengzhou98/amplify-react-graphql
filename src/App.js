c4707228dc5a55609ab52c017bfe02f155cbf539
renren <renren@zhichengzhou-MacBook-Air.local>
2023-01-04 16:44:30 +0800

    file upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 import { listTodos } from './graphql/queries';
 import { createTodo , deleteTodo } from './graphql/mutations';
 
-import { API } from 'aws-amplify';
+
+import { API, Storage } from 'aws-amplify';
 
 const initialFormState = { name: '', description: '' }
 
@@ -18,15 +19,37 @@ function App() {
   useEffect(() => {
     fetchNotes();
   }, []);
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
+
 
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listTodos });
+    const notesFromAPI = apiData.data.listTodos.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
     setNotes(apiData.data.listTodos.items);
   }
+
+
 
   async function createNote() {
     if (!formData.name || !formData.description) return;
     await API.graphql({ query: createTodo, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
@@ -65,6 +88,22 @@ function App() {
           ))
         }
       </div>
+      <input
+  type="file"
+  onChange={onChange}
+/>
+{
+  notes.map(note => (
+    <div key={note.id || note.name}>
+      <h2>{note.name}</h2>
+      <p>{note.description}</p>
+      <button onClick={() => deleteNote(note)}>Delete note</button>
+      {
+        note.image && <img src={note.image} style={{width: 400}} />
+      }
+    </div>
+  ))
+}
 
       {/* <button onClick={signOut}>login out</button> */}
     </div>
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -27,7 +27,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://j27ret32ebc2fbsmgszb5awxhe.appsync-api.us-west-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-6rivedaqlfdvxc67oprjuvph6y"
+    "aws_appsync_apiKey": "da2-6rivedaqlfdvxc67oprjuvph6y",
+    "aws_user_files_s3_bucket": "amplifyappmyfilebucket111839-dev",
+    "aws_user_files_s3_bucket_region": "us-west-1"
 };
 
 
